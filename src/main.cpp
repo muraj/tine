17add8cd5a51d284b74a59d660049f0219f3e6a6
Cory Perry <killogg@gmail.com>
2023-01-02 01:02:49 -0800

    Fix github actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#define GLAD_VULKAN_IMPLEMENTATION 1
+#include <vulkan/vulkan.h>
 #include <iostream>
 
 int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
     std::cout << "Hello, World!\n";
     return 0;
 }
